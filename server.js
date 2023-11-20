c7e6b108523bd84df008491a1c1f211239acb38d
JoealChrish <jc6238@srmist.edu.in>
2023-11-20 19:05:24 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const cluster = require('cluster')
+const os = require('os')
+const app = express()
+
+const numCpu = os.cpus().length
+
+app.get('/', (req, res) => {
+    for(let i=0; i<1e8; i++){
+
+    }
+    res.send(`okkk... ${process.pid}`)
+    cluster.worker.kill()
+})
+
+if(cluster.isMaster){
+    for (let i = 0; i < numCpu; i++) {
+        cluster.fork();
+      }
+      cluster.on('exit',(worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`)
+        cluster.fork()
+      })
+} else {
+    app.listen(3000, () => 
+        console.log(`Server ${process.pid} is running on 3000`)
+    )
+}
+
+
+
+
+
